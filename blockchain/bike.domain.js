fc127a8c3027ee5ca002828ef1725262fc2ba889
Jonathan Massuchetti <jonathan.massuchetti@xxii.fr>
2019-08-26 16:17:54 +0200

    fix bike domain validation error building message

--- blockchain/bike.domain.js
+++ blockchain/bike.domain.js
@@ -16,14 +16,14 @@ module.exports = {
         }
     },
     BikeValidator: {
-        id: (transactionId, param) => (param || typeof param === 'string' || param.length > 0) ? true :
-            new TransactionError('Missing or invalid "asset.id" defined on transaction', transactionId, '.asset.id', this.asset.id, 'A string'),
+        id: (transactionId, param) => (param && typeof param === 'string' && param.length > 0) ? true :
+            new TransactionError('Missing or invalid "asset.id" defined on transaction', transactionId, '.asset.id', param, 'A string'),
         pricePerHour: (transactionId, param) => {
             try {
                 const pricePerHour = new BigNum(param);
                 return pricePerHour.gte(0);
             } catch(err) {
-                return new TransactionError('Missing or invalid "asset.pricePerHour" defined on transaction', transactionId, '.asset.pricePerHour', this.asset.pricePerHour, 'A valid BigNum string')
+                return new TransactionError('Missing or invalid "asset.pricePerHour" defined on transaction', transactionId, '.asset.pricePerHour', param, 'A valid BigNum string')
             }
         },
         deposit: (transactionId, param) => {
@@ -31,7 +31,7 @@ module.exports = {
                 const deposit = new BigNum(param);
                 return deposit.gte(0);
             } catch(err) {
-                return new TransactionError('Missing or invalid "asset.deposit" defined on transaction', transactionId, '.asset.deposit', this.asset.deposit, 'A valid BigNum string')
+                return new TransactionError('Missing or invalid "asset.deposit" defined on transaction', transactionId, '.asset.deposit', param, 'A valid BigNum string')
             }
         }
     }
