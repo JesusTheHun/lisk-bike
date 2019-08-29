079b28d59a298758e7d6c87cbb8a45a6d608952a
Jonathan Massuchetti <jonathan.massuchetti@xxii.fr>
2019-08-30 01:35:43 +0200

    add fixtures to reset

--- blockchain/reset-docker-compose.sh
+++ blockchain/reset-docker-compose.sh
@@ -5,3 +5,6 @@ docker-compose start psql
 docker exec -ti lisk-psql psql -h localhost -U lisk -d postgres -c "DROP DATABASE lisk_dev"
 docker exec -ti lisk-psql psql -h localhost -U lisk -d postgres -c "CREATE DATABASE lisk_dev OWNER lisk"
 docker-compose start nodejs
+cd tests
+node create-accounts.test.js
+node create-bikes.test.js 9
